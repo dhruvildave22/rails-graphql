67702f65b413f3d44c83f0bf21bb8d71cf1ef224
Dhruvil Dave <dhruvildave22@gmail.com>
2021-04-19 10:18:52 +0530

    list data using graphql

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Library from '../components/Library';
+import Provider from '../components/Provider';
+
+render(<Provider><Library /></Provider>, document.querySelector('#root'));
